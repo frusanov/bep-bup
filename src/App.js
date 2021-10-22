e8a998823a36e69034afea03110c114459a76cc4
Feodor Rusanov <f.rusanov@gmail.com>
2021-10-22 06:15:58 +0300

    bep bup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // https://twitter.com/marcinignac/status/1288418586709630976
 
 import * as THREE from 'three'
-import React, { useState, useRef, Suspense } from 'react'
+import React, { useState, useRef, Suspense, useEffect } from 'react'
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
-import { softShadows, BakeShadows, RoundedBox, Environment, useCursor } from '@react-three/drei'
+import { softShadows, BakeShadows, RoundedBox, Environment, useCursor, ContactShadows } from '@react-three/drei'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { Text, CameraShake } from '@react-three/drei'
 
 // Soft shadows are expensive, uncomment and refresh when it's too slow
-// softShadows()
+softShadows()
 
 function damp(target, to, step, delta, v = new THREE.Vector3()) {
   if (target instanceof THREE.Vector3) {
@@ -16,34 +16,6 @@ function damp(target, to, step, delta, v = new THREE.Vector3()) {
     target.y = THREE.MathUtils.damp(target.y, to[1], step, delta)
     target.z = THREE.MathUtils.damp(target.z, to[2], step, delta)
   }
-}
-
-function Button({ v = new THREE.Vector3(), c = new THREE.Color() }) {
-  const material = useRef()
-  const [active, setActive] = useState(false)
-  const [zoom, set] = useState(true)
-  useCursor(active)
-  useFrame((state, delta) => {
-    const step = 4
-    state.camera.fov = THREE.MathUtils.damp(state.camera.fov, zoom ? 10 : 42, step, delta)
-    damp(state.camera.position, [zoom ? 25 : 10, zoom ? 1 : 5, zoom ? 0 : 10], step, delta)
-    state.camera.lookAt(0, 0, 0)
-    state.camera.updateProjectionMatrix()
-  })
-  return (
-    <mesh receiveShadow castShadow onClick={() => set(!zoom)} onPointerOver={() => setActive(true)} onPointerOut={() => setActive(false)}>
-      <sphereGeometry args={[0.8, 64, 64]} />
-      <meshPhysicalMaterial
-        ref={material}
-        clearcoat={1}
-        clearcoatRoughness={0}
-        transmission={1}
-        thickness={1.1}
-        roughness={0}
-        envMapIntensity={2}
-      />
-    </mesh>
-  )
 }
 
 function Plane({ color, ...props }) {
@@ -54,30 +26,86 @@ function Plane({ color, ...props }) {
   )
 }
 
-const Pixie = () => {
-  const gltf = useLoader(GLTFLoader, '/models/pixie-frog/scene.gltf')
+function Pixie(props) {
+  const gltf = useLoader(GLTFLoader, '/models/pixie.gltf')
+  const [rotata, setRotata] = useState(0)
+
+  useEffect(() => {
+    let lastRotata = 0
+
+    const intertval = setInterval(() => {
+      lastRotata = lastRotata + 0.01
+      setRotata(lastRotata)
+    }, 1000 / 60)
+
+    return () => clearInterval(intertval)
+  }, [])
 
   return (
-    <>
-      <primitive object={gltf.scene} scale={0.4} />
-    </>
+    <mesh rotation={[0, rotata, -0.02]} {...props}>
+      <primitive object={gltf.scene} />
+    </mesh>
   )
+}
+
+function Pixie2(props) {
+  const gltf = useLoader(GLTFLoader, '/models/pixie.gltf?2')
+
+  return (
+    <mesh {...props}>
+      <primitive object={gltf.scene} />
+    </mesh>
+  )
+}
+
+function Hack() {
+  const [zoom, set] = useState(true)
+
+  useFrame((state, delta) => {
+    const step = 1
+    state.camera.fov = THREE.MathUtils.damp(state.camera.fov, zoom ? 8 : 42, step, delta)
+
+    damp(state.camera.position, [zoom ? 25 : 10, zoom ? 1 : 2, zoom ? 0 : 10], step, delta)
+
+    state.camera.lookAt(0, 2, 0)
+    state.camera.updateProjectionMatrix()
+  })
+
+  useEffect(() => {
+    setTimeout(() => {
+      set(false)
+    }, 1000)
+  })
+
+  return <></>
 }
 
 export default function App() {
   return (
-    <Canvas shadows camera={{ position: [20, 15, 50], fov: 42 }}>
+    <Canvas shadows camera={{ position: [25, 1, 0], fov: 2 }}>
       <color attach="background" args={['#a2b9e7']} />
+      <Hack />
       <directionalLight position={[0, 8, 5]} castShadow intensity={1} shadow-camera-far={70} />
       <Suspense fallback={null}>
+        <Pixie scale={0.1} position={[0.05, -1.1, 0]} />
+        <Pixie2 scale={20} position={[-60, -180, -40]} rotation={[0, -1.2, 0.25]} />
+        <Text
+          position={[-21.5, 5, -20]}
+          color={'#ff0f0f'}
+          fontSize={4}
+          maxWidth={20}
+          lineHeight={1}
+          letterSpacing={0.02}
+          textAlign={'left'}
+          font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+          anchorX="center"
+          anchorY="middle">
+          С Днем Рождения! Бепа! Бупа!
+        </Text>
         <group position={[0, -0.9, -3]}>
-          {/* <Pixie /> */}
-          <Plane color="hotpink" rotation-x={-Math.PI / 2} position-z={2} scale={[4, 40, 0.2]} />
-          <Plane color="#f4ae00" rotation-x={-Math.PI / 2} position-y={1} scale={[4.2, 0.2, 4]} />
-          <Plane color="#436fbd" rotation-x={-Math.PI / 2} position={[-1.7, 1, 3.5]} scale={[0.5, 4, 4]} />
-          <Plane color="#d7dfff" rotation-x={-Math.PI / 2} position={[0, 4.5, 3]} scale={[2, 0.03, 4]} />
+          <Plane color="#ea5b3f" rotation-x={-Math.PI / 2} position-z={2} scale={[5, 40, 0.2]} />
+          <Plane color="#f4ae00" rotation-x={-Math.PI / 2} position={[0, 0, 0]} scale={[4.2, 0.2, 4]} />
         </group>
-        <Button />
         <Environment preset="apartment" />
         <BakeShadows />
       </Suspense>
